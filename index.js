24b5319da816c183ef46518d4b95fb10256558e3
SOFIA-ASIF <160203172+SOFIA-ASIF@users.noreply.github.com>
2024-05-10 20:09:57 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let wordsSentence = await inquirer.prompt([{
+        name: "choices",
+        type: "list",
+        message: "Do you want to count alphabets or words??",
+        choices: ['Alphabets', 'Words']
+    }, {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence"
+    }]);
+if (wordsSentence.choices == 'Alphabets') {
+    const alphabets = wordsSentence.sentence.trim().split("").filter(bets => bets != " ");
+    console.log(alphabets.length);
+}
+else if (wordsSentence.choices == "Words") {
+    const words = wordsSentence.sentence.trim().split(" ");
+    console.log(words.length);
+}
